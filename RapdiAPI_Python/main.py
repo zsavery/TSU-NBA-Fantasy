f8da48345764f8c5fbcaa7633f89e3181fda7191
zsavery <zsavery998@gmail.com>
2021-04-19 21:57:52 -0500

    Combine Hunter's and Zyon's code. Still need to add player name stats data frame

--- RapdiAPI_Python/main.py
+++ RapdiAPI_Python/main.py
@@ -6,6 +6,7 @@ import rapidapi_nba
 # %%
 
 if __name__ == "__main__":
+    pd.set_option("display.max_rows", None, "display.max_columns", None)
     # header
     head = {
         'x-rapidapi-key': "9ab5f1799cmsh27212c7d8f60efep17f457jsn395ab0f973b6",
@@ -36,8 +37,7 @@ if __name__ == "__main__":
     # %%
 
     player_ids_lst = [x for x in active_players_name_ids["playerId"]]
-    print(player_ids_lst)
-    # print list of player_
+
 
     # %%
 
@@ -46,13 +46,14 @@ if __name__ == "__main__":
 
     # %%
     # Set up table for that holds stats that will be displayed
-    column_names = ['playerId', 'teamId', 'pos', 'points', 'totReb', 'assists',
+    column_names = ['playerId', 'firstName', 'lastName', 'teamId', 'pos', 'points', 'totReb', 'assists',
                     'steals', 'blocks', 'turnovers', 'fantasyPoints']
     average_stats = pd.DataFrame(columns=column_names)
 
     # %%
     # Get stats from player from player with id '265' ie LeBron James
-    player_stats_r = rapidapi_nba.get_player_stats_by_player_id(head, '265')
+    Id = '265'
+    player_stats_r = rapidapi_nba.get_player_stats_by_player_id(head, Id)
     player_stats = player_stats_r.json()['api']['statistics']
     stat_df = pd.json_normalize(player_stats)
     if len(player_stats) >= 5:
@@ -61,13 +62,15 @@ if __name__ == "__main__":
         latest_stat_df = pd.json_normalize(player_stats)
 
     # print(type(latest_stat_df['points'][0]))
-
+    x = active_players_name_ids.loc[active_players_name_ids['playerId'] == Id]
     latest_stat_df[["points", "totReb", "assists", "steals", "turnovers", "blocks"]] = latest_stat_df[
         ["points", "totReb", "assists", "steals", "turnovers", "blocks"]].apply(pd.to_numeric)
 
 #     # TODO: Add Player name to columns
 #     # print(type(latest_stat_df['points'][0]))
     average_stats = average_stats.append({'playerId': latest_stat_df['playerId'][0],
+                                          'firstName': x["firstName"],
+                                         'lastName': x["lastName"],
                                           'teamId': latest_stat_df['teamId'][0],
                                           'pos': latest_stat_df['pos'][0],
                                           'points': latest_stat_df['points'].mean(),
@@ -101,11 +104,13 @@ if __name__ == "__main__":
 #             latest_stat_df = pd.json_normalize(player_stats[-6:-1])
 #         else:
 #             latest_stat_df = pd.json_normalize(player_stats)
-#
+#         x = active_players_name_ids.loc[active_players_name_ids['playerId'] == Id]
 #         latest_stat_df[["points", "totReb", "assists", "steals", "turnovers", "blocks"]] = latest_stat_df[
 #             ["points", "totReb", "assists", "steals", "turnovers", "blocks"]].apply(pd.to_numeric)
 #
 #         average_stats = average_stats.append({'playerId': latest_stat_df['playerId'][0],
+#                                               'firstName': x.firstName,
+#                                               'lastName': x.lastName,
 #                                               'teamId': latest_stat_df['teamId'][0],
 #                                               'pos': latest_stat_df['pos'][0],
 #                                               'points': latest_stat_df['points'].mean(),
@@ -113,16 +118,16 @@ if __name__ == "__main__":
 #                                               'assists': latest_stat_df['assists'].mean(),
 #                                               'steals': latest_stat_df['steals'].mean(),
 #                                               'blocks': latest_stat_df['blocks'].mean(),
-#                                               'turnovers':latest_stat_df['turnovers'].mean(),
+#                                               'turnovers': latest_stat_df['turnovers'].mean(),
 #                                               'fantasyPoints': ((latest_stat_df['points'].mean() * 1) +
 #                                                                 (latest_stat_df['totReb'].mean() * 1.2) +
 #                                                                 (latest_stat_df['assists'].mean() * 1.5) +
 #                                                                 (latest_stat_df['steals'].mean() * 2) +
 #                                                                 (latest_stat_df['blocks'].mean() * 2) +
-#                                                                 (latest_stat_df['turnovers'].mean() * -1))},
+#                                                                 (latest_stat_df['turnovers'].mean() * -1))
+#                                               },
 #                                              ignore_index=True)
 #         completed_player += 1
 #         print(f"Completed player {completed_player} of {len(player_ids_lst)}")
 #     print(average_stats)
-
 
