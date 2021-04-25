a68f69652de08505e8a5f18d9d259f66908bf432
zsavery <zsavery998@gmail.com>
2021-04-24 19:20:55 -0500

    add top5 in mysql

--- GetData/create_db.py
+++ GetData/create_db.py
@@ -15,7 +15,7 @@ def to_mysql(df):
     database = "stats_test"
     # grant_privileges = f"grant all privileges on {cert['host']}.{database} to `{cert['user']}`@`localhost`;"
     # conn.cursor().execute(grant_privileges)
-    conn.cursor().execute(f"CREATE DATABASE IF NOT EXISTS {database}")
+    conn.cursor().execute(f"CREATE DATABASE IF NOT EXISTS {database};")
     conn = pymysql.connect(host=cert["host"],
                            port=cert["port"],
                            user=cert["user"],
@@ -47,15 +47,43 @@ def to_mysql(df):
         if "'" in lastName:
             lastName = lastName.replace("'", "''")
 
-        query = (f"""
+        update_table = (f"""
         REPLACE INTO {database}.{table} (playerId, firstName, lastName, teamId, pos, points,
             totReb, assists, steals, blocks, turnovers, fantasyPoints) VALUES
             ('{playerId}', '{firstName}', '{lastName}', '{teamId}', '{pos}', {points}, {totReb}, {assists}, {steals}, {blocks}, {turnovers}, {fantasyPoints});""")
-        print(query)
+        print(update_table)
+        conn.cursor().execute(update_table)
 
-        conn.cursor().execute(query)
         print(f"Add playerId: {playerId}")
-        conn.commit()
+    conn.commit()
+    top_n(conn, table)
+    return
+
+
+def top_n(conn, table1):
+    table2 = "top5"
+    n = 5
+    selected_column = "fantasyPoints"
+
+    drop_table = f'''
+    DROP TABLE IF EXISTS {table2};
+    '''
+    conn.cursor().execute(drop_table)
+
+    create_table2_like_table1 = f'''
+    CREATE TABLE IF NOT EXISTS {table2} LIKE {table1};'''
+    conn.cursor().execute(create_table2_like_table1)
+
+    get_top_n_from_table1 = f'''
+    SELECT playerId, firstName, lastName, teamId, pos, points, totReb, assists, steals, blocks, turnovers, fantasyPoints
+    FROM {table1}
+    ORDER BY {selected_column} DESC
+    LIMIT {n};
+    '''
+    conn.cursor().execute(get_top_n_from_table1)
+    conn.commit()
+
+    return
 
 
 def drop():
